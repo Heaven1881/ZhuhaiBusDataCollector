aca497616081c6dcea3be11ca69201f3b2130657
Heaven1881 <Heaven1881@163.com>
2018-01-17 20:08:48 +0800

    rename BusDataCollector to BusDataRequest

--- test/BusDataCollectorTest.py
+++ test/BusDataCollectorTest.py
@@ -4,7 +4,7 @@
 import unittest
 import json
 
-import BusDataCollector
+import BusDataRequest
 
 
 class TestBusDataRequest(unittest.TestCase):
@@ -22,18 +22,18 @@ class TestBusDataRequest(unittest.TestCase):
                 raise self.mock_exception
             return self.mock_result
 
-        self.__http_get_backup = BusDataCollector.g_http_get
-        BusDataCollector.g_http_get = mock_http_get
+        self.__http_get_backup = BusDataRequest.g_http_get
+        BusDataRequest.g_http_get = mock_http_get
 
     def tearDown(self):
-        BusDataCollector.g_http_get = self.__http_get_backup
+        BusDataRequest.g_http_get = self.__http_get_backup
 
 
 class TestBusLineListRequest(TestBusDataRequest):
 
     def test_init(self):
         """测试初始化"""
-        request = BusDataCollector.BusLineListRequest('A')
+        request = BusDataRequest.BusLineListRequest('A')
 
         self.assertFalse(request.data_fetched)
         self.assertFalse(any(request.data))
@@ -45,7 +45,7 @@ class TestBusLineListRequest(TestBusDataRequest):
             'key': 'value'
         })
 
-        request = BusDataCollector.BusLineListRequest('3A')
+        request = BusDataRequest.BusLineListRequest('3A')
         request.fetch_data()
 
         self.assertTrue('key=3A' in self.passed_args_url)
@@ -59,7 +59,7 @@ class TestBusLineListRequest(TestBusDataRequest):
         """测试空返回值"""
         self.mock_result = ''
 
-        request = BusDataCollector.BusLineListRequest('3A')
+        request = BusDataRequest.BusLineListRequest('3A')
         request.fetch_data()
 
         self.assertFalse(request.data_fetched)
@@ -69,7 +69,7 @@ class TestBusLineListRequest(TestBusDataRequest):
         """测试返回值错误"""
         self.mock_result = '123456'
 
-        request = BusDataCollector.BusLineListRequest('3A')
+        request = BusDataRequest.BusLineListRequest('3A')
         request.fetch_data()
 
         self.assertFalse(request.data_fetched)
@@ -77,7 +77,7 @@ class TestBusLineListRequest(TestBusDataRequest):
 
     def test_double_success_fetch_data(self):
         """如果请求已经成功，重复调用不会再次触发网络请求"""
-        request = BusDataCollector.BusLineListRequest('3A')
+        request = BusDataRequest.BusLineListRequest('3A')
         request.fetch_data()
         request.fetch_data()
 
@@ -87,7 +87,7 @@ class TestBusLineListRequest(TestBusDataRequest):
         """失败的情况下可以重试"""
         self.mock_exception = Exception("Mock Exception")
 
-        request = BusDataCollector.BusLineListRequest('3A')
+        request = BusDataRequest.BusLineListRequest('3A')
         request.fetch_data()
         request.fetch_data()
 
